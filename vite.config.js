16890921f52d923009669ea78af09eab16e6a9e1
erye <erye@tiiit.cn>
2022-06-11 20:48:42 +0800

    更新构建配置
    1.导出2套文件，一份包含sourcemap配置，一份不包含；
    2.更新scripts，使用rimraf清空目录

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,4 @@
 import path from "path";
-import fs from "fs";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
@@ -7,61 +6,79 @@ import vueJsx from "@vitejs/plugin-vue-jsx";
 import VitePluginVafMockAuth from "./vite-plugin-vaf-mock-auth";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  root: path.resolve(__dirname, "./demo"),
-  base: "/",
-  plugins: [
-    vue(),
-    vueJsx({
-      // options are passed on to @vue/babel-plugin-jsx
-    }),
-    VitePluginVafMockAuth(),
-  ],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@import "@/common/scss/index.scss";`,
+export default defineConfig(({ command, mode }) => {
+  console.log(command, mode);
+
+  return {
+    root: path.resolve(__dirname, "./demo"),
+    base: "/",
+    plugins: [
+      vue(),
+      vueJsx({
+        // options are passed on to @vue/babel-plugin-jsx
+      }),
+      VitePluginVafMockAuth(),
+    ],
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@import "@/common/scss/index.scss";`,
+        },
+      },
+      devSourcemap: true,
+    },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
       },
     },
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, "src/index.js"),
-      name: "Vaf",
-      fileName: (format) => `vaf.${format}.js`,
-    },
-    emptyOutDir: true,
-    rollupOptions: {
-      // 确保外部化处理那些你不想打包进库的依赖
-      external: [
-        "axios",
-        "element-plus",
-        "nprogress",
-        "vue",
-        "vue-router",
-        "vuex",
-        "vuex-persistedstate",
-      ],
-      output: {
+    build: {
+      lib: {
+        entry: path.resolve(__dirname, "src/index.js"),
         name: "Vaf",
-        exports: "named",
-        dir: path.resolve(__dirname, "./dist"),
-        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
-        globals: {
-          axios: "Vue",
-          "element-plus": "ElementPlus",
-          nprogress: "NProgress",
-          vue: "Vue",
-          "vue-router": "VueRouter",
-          vuex: "Vuex",
-          "vuex-persistedstate": "createPersistedState",
+        fileName: (format) => {
+          const withMin = "build:development" === mode ? "" : ".min";
+          return `vaf.${format + withMin}.js`;
+        },
+      },
+      polyfillModulePreload: false,
+      emptyOutDir: false,
+      sourcemap: "build:development" === mode,
+      rollupOptions: {
+        // 确保外部化处理那些你不想打包进库的依赖
+        external: [
+          "axios",
+          "element-plus",
+          "nprogress",
+          "vue",
+          "vue-router",
+          "vuex",
+          "vuex-persistedstate",
+        ],
+        output: {
+          name: "Vaf",
+          exports: "named",
+          dir: path.resolve(__dirname, "./dist"),
+          // Library Mode: support custom output css file name #8115
+          // https://github.com/vitejs/vite/issues/8115
+          assetFileNames: (chunkInfo) => {
+            if (chunkInfo.name === "style.css") {
+              const withMin = "build:development" === mode ? "" : ".min";
+              return `index${withMin}.css`;
+            }
+          },
+          // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+          globals: {
+            axios: "Vue",
+            "element-plus": "ElementPlus",
+            nprogress: "NProgress",
+            vue: "Vue",
+            "vue-router": "VueRouter",
+            vuex: "Vuex",
+            "vuex-persistedstate": "createPersistedState",
+          },
         },
       },
     },
-  },
+  };
 });
